9477673940b7667885aadd9ad267e3d4075ff317
ShubhamMehra17 <shubham.mehra1712@gmail.com>
2026-01-15 18:11:57 +0530

    Update trivyScan.groovy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,5 +1,27 @@
-def call(String imageName, String tag) {
+def call(Map config = [:]) {
+
+    def imageName = config.imageName ?: error("imageName is required")
+    def imageTag  = config.imageTag ?: "latest"
+    def severity  = config.severity ?: "CRITICAL,HIGH"
+    def failBuild = config.failBuild ?: true
+    def trivyUrl  = config.trivyUrl ?: "http://localhost:4954"
+
+    echo "🔍 Trivy Image Scan "
+    echo "Image    : ${imageName}:${imageTag}"
+    echo "Server   : ${trivyUrl}"
+    echo "Severity : ${severity}"
+
+    def exitCode = failBuild ? "1" : "0"
+
     sh """
-        trivy image --exit-code 0 --severity HIGH,CRITICAL ${imageName}:${tag}
+      docker run --rm \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        aquasec/trivy:latest image \
+        --server ${trivyUrl} \
+        --severity ${severity} \
+        --exit-code ${exitCode} \
+        ${imageName}:${imageTag}
     """
+
+    echo "✅ Trivy scan completed"
 }
